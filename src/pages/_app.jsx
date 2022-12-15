92ba66f6a76a2e06016057a4dcadc2b32375a3a6
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-12-15 01:19:15 -0300

    Add metadata for viewport

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,6 +6,10 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta charSet={'utf-8'} />
         <meta httpEquiv={'X-UA-Compatible'} content={'IE=edge'} />
+        <meta
+          name={'viewport'}
+          content={'width=device-width, initial-scale=1.0'}
+        />
       </Head>
       <Component {...pageProps} />
     </>
