2792cb332509994c9d82e5e3432ee0ea4bce346f
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-12-17 13:09:06 -0300

    Set header title to application product string from config file

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,11 @@
 import Title from 'src/components/title'
+import appConfig from 'roadmap-config'
 
 export default function Header() {
+  const productName = appConfig.app.product
   return (
     <div>
-      <Title />
+      <Title title={productName} />
     </div>
   )
 }
