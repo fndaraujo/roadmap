2b724c12e513c7ef660c89303fd39853f3a6788c
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-12-17 21:49:14 -0300

    Add fetch API data in guide pages

--- src/pages/[guide].jsx
+++ src/pages/[guide].jsx
@@ -1,9 +1,26 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export default function Guide() {
   const router = useRouter()
   const { guide } = router.query
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(`/api/${guide}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [guide])
+
+  if (isLoading) return <p>Loading...</p>
+  if (!data) return <p>No profile data</p>
+
   return (
     <div>
       <Head>
