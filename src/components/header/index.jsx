af9a53dc70f8ef317531d08b41d541026a540b27
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-12-17 21:19:49 -0300

    Add header links to guide routes

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,15 @@
 import Title from 'src/components/title'
 import appConfig from 'roadmap-config'
+import Link from 'next/link'
 
 export default function Header() {
   const productName = appConfig.app.product
   return (
     <div>
       <Title title={productName} />
+      <Link href={'/aspnet'}>aspnet core</Link>
+      <Link href={'/dotnet'}>dotnet core</Link>
+      <Link href={'/reactjs'}>react js</Link>
     </div>
   )
 }
