50090726e5ec1a0fa395b6c9e8e93e1f9e8ed58e
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-12-18 16:26:59 -0300

    Add a link to the about page in the header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,6 +7,7 @@ export default function Header() {
   return (
     <div>
       <Title title={productName} />
+      <Link href={'/about'}>about</Link>
       <Link href={'/aspnet'}>aspnet core</Link>
       <Link href={'/dotnet'}>dotnet core</Link>
       <Link href={'/reactjs'}>react js</Link>
