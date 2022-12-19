913e5c06d2588ae76704d5747c45baaf781f4d60
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-12-18 21:49:56 -0300

    Create header component stylesheet file

---
+++ src/components/header/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  ul {
+    display: flex;
+    gap: 1rem;
+  }
+`
